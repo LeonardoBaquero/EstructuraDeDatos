1cf3aaba82f6a8e0c430ab95be6f912ecb74c9f3
Leonardo Baquero <leonardo.baquero@utp.edu.co>
2018-08-13 11:14:04 -0500

    Pruebas de funcionamiento de junit.

--- src/MontoEscrito.java
+++ src/MontoEscrito.java
@@ -1,7 +1,5 @@
 import java.util.Scanner; //Se agrega el paquete java.until para usar la clase Scanner que permite leer valores de entrada
 
-import java.util.Scanner;
-
 public class MontoEscrito {
 	
 	public static void main(String[] args) {
