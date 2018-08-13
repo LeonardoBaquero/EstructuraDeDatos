f57d94801ca40a2120693af0d29f91864234d9e4
Leonardo Baquero <leonardo.baquero@utp.edu.co>
2018-08-12 21:23:32 -0500

    Organizando y probando el código del 0 al 100

--- src/MontoEscrito.java
+++ src/MontoEscrito.java
@@ -8,26 +8,23 @@ public class MontoEscrito {
 		// TODO Auto-generated method stub
         
         String unidades[] = 
-        {"cero", "uno", "dos" ,"tres" ,"cuatro" ,"cinco" ,
-            "seis" ,"siete" ,"ocho" ,"nueve","diez"};
-        String especiales[] = 
-        {"once", "doce","trece","catorce", "quince", 
-            "diezciseis", "diecisiete", "dieciocho", "diecinueve"};
+        {"cero", "uno", "dos" ,"tres" ,"cuatro" ,"cinco" , "seis" ,"siete" ,"ocho" ,"nueve","diez"};
+        String decenas11al19[] = 
+        {"once", "doce","trece","catorce", "quince", "diezciseis", "diecisiete", "dieciocho", "diecinueve"};
         String decenas[] = 
-        {"veinte", "treinta","cuarenta","cincuenta", "sesenta",
-            "setenta", "ochenta", "noventa"};
+        {"veinte", "treinta","cuarenta","cincuenta", "sesenta", "setenta", "ochenta", "noventa"};
         /*String centenas[] = 
         {"cien", "doscientos","trescientos","cuatrocientos", 
         "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"};*/
                              
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
         System.out.print("Ingrese un numero entre 0 y 99: "); 
         int num = scanner.nextInt();
         
         if(num>=0 && num<11)
              System.out.print('\n' + unidades[num]);         
         else if(num<20)
-            System.out.print('\n' + especiales[num-11]);        
+            System.out.print('\n' + decenas11al19[num-11]);        
          else if(num<100){
             int unid = num % 10;
             int dec = num/10;
