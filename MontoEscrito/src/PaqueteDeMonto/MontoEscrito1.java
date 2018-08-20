07dfab42dea0364346a8cb8938d2f7eae46b2857
Leonardo Baquero <leonardo.baquero@utp.edu.co>
2018-08-20 16:47:39 -0500

    Segunda prueba de junit.

--- MontoEscrito/src/PaqueteDeMonto/MontoEscrito1.java
+++ MontoEscrito/src/PaqueteDeMonto/MontoEscrito1.java
@@ -1,45 +1,55 @@
 package PaqueteDeMonto;
+
 import java.text.NumberFormat; // Esta clase proporciona la interfaz para formatear y analizar números.
 import java.util.Scanner; // Clase para lectura de datos 
 
 public class MontoEscrito1 {
 
-public static final String[] unidades = { "", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "once", "doce",
-"trece", "catorce", "quince", "dieciseis", "diecisiete", "dieciocho", "diecinueve" };
+	public static final String[] unidades = { "", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho",
+			"nueve", "diez", "once", "doce", "trece", "catorce", "quince", "dieciseis", "diecisiete", "dieciocho",
+			"diecinueve" };
 
-public static final String[] decenas = { 
-	    "",	// 0
-		"",	// 1
-		"veinti", "treinta", "cuareta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa"};
+	public static final String[] decenas = { "", // 0
+			"", // 1
+			"veinti", "treinta", "cuareta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
 
-public static String convert(final int n) {
-	if (n < 0) {
-		return "Menos" + convert(-n);
-	}
+	public static String convert(final int n) {
+		if (n < 0) {
+			return "Menos" + convert(-n);
+		}
 
-	if (n < 20) {
-		return unidades[n];
-		
-	}
+		if (n < 20) {
+			return unidades[n];
 
-	if (n < 100) {
-		return decenas[n / 10] + ((n % 10 != 0)?"": "") + unidades[n % 10];
-	}
+		}
 
-	if (n < 1000) {
-		return unidades[n / 100] + " cien" + ((n % 100 != 0)? " " : "") + convert(n % 100);
-	}
+		if (n < 100) {
+			return decenas[n / 10] + ((n % 10 != 0) ? "" : "") + unidades[n % 10];
+			// Se utiliza el operador terniario '?'
+		}
+
+		if (n < 1000) {
+			return unidades[n / 100] + " cien" + ((n % 100 != 0) ? " " : "") + convert(n % 100);
+		}
 		return convert(n / 1000) + " mil" + ((n % 10000 != 0) ? " " : "") + convert(n % 1000);
 	}
 
-public static void main(final String[] args) {
+	public static void main(final String[] args) {
 
-	int n;
-	Scanner numero=new Scanner(System.in);
-    System.out.println("Ingrese el numero que desea ver en letras");
-	n =numero.nextInt();
-	System.out.println(NumberFormat.getInstance().format(n) + "=" + convert(n) + "'");
+		int n;
+		Scanner numero = new Scanner(System.in);
+		System.out.println("Ingrese el numero que desea ver en letras");
+		n = numero.nextInt();
+		System.out.println(NumberFormat.getInstance().format(n) + "=" + convert(n) + "'");
 
-}
+	}
+
+	public static String[] getUnidades() {
+		return unidades;
+	}
+
+	public static String[] getDecenas() {
+		return decenas;
+	}
 
 }
---
+++ MontoEscrito/src/Test/MontoEscrito1Test.java
@@ -0,0 +1,23 @@
+ package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import PaqueteDeMonto.MontoEscrito1;
+
+class MontoEscrito1Test {
+
+	@Test
+	void testGetUnidades() {
+		//Paso  1: Instansear la clase a probar.
+		MontoEscrito1 testedMonto = new MontoEscrito1();   
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDecenas() {
+		fail("Not yet implemented");
+	}
+
+}
